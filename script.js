879bc559629681f56037edd47985de3952d30635
Taranveer Singh <154905330+Taran677@users.noreply.github.com>
2024-06-05 10:39:55 +0530

    asynchronous functions added

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
 
-fetch("https://icanhazdadjoke.com/", {
-  method: "GET",
-  headers: myHeaders,
-})
-  .then((res) => {
-    console.log("Resolved.");
-    return res.json();
-  })
-  .then((data) => {
-    const jokeCont = document.getElementsByClassName("joke-cont")[0];
-    jokeCont.innerHTML = data.joke;
-  })
-  .catch((err) => {
-    console.log("Error!", err);
-  });
+const fetchData = async () => {
+  try {
+    let response = await fetch("https://icanhazdadjoke.com/", {
+      method: "GET",
+      headers: myHeaders
+    });
+    let data = await response.json();
+    return data;
+  } catch (err) {
+    console.log("error: ", err);
+  }
+};
+
+const displayJoke = (data) => {
+  const jokeContainer = document.querySelector(".joke-cont"); 
+  jokeContainer.textContent = data.joke;
+};
+
+fetchData().then(displayJoke);
